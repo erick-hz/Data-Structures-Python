79c974abd5e3a3e6e1d729be4fcec2ae211b81e9
Erick Hernandez <yerickk8@gmail.com>
2024-06-05 13:36:22 -0600

    Update hashtable.py
    test

--- DataStructures/Hash Table/hashtable.py
+++ DataStructures/Hash Table/hashtable.py
@@ -1,4 +1,3 @@
-# create a dictionary to store information about fruits
 fruit_info = {'apple': {'color': 'red', 'price': 0.50},
               'banana': {'color': 'yellow', 'price': 0.25},
               'orange': {'color': 'orange', 'price': 0.30}}
@@ -6,16 +5,12 @@ fruit_info = {'apple': {'color': 'red', 'price': 0.50},
 # add a new fruit to the dictionary
 fruit_info['pear'] = {'color': 'green', 'price': 0.40}
 
-# access information about a specific fruit
-print(fruit_info['apple']['color'])  # output: red
-print(fruit_info['banana']['price'])  # output: 0.25
 
-# loop over all the keys in the dictionary
+print(fruit_info['apple']['color']) 
+print(fruit_info['banana']['price']) 
+
+
 for fruit in fruit_info.keys():
     print(fruit, fruit_info[fruit]['price'])
-    
-# output:
-# apple 0.5
-# banana 0.25
-# orange 0.3
-# pear 0.4
+
+
