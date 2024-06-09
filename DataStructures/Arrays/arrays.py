db9abe91820cdff41068b8e92e0d0c313d1b51bf
Erick Hernandez <yerickk8@gmail.com>
2024-06-09 09:45:07 -0600

    Arrays
    Added arrays
    
    Co-Authored-By: verozzf22 <112897590+verozzf22@users.noreply.github.com>
    Co-Authored-By: pixelbuter <115113296+pixelbuter@users.noreply.github.com>

---
+++ DataStructures/Arrays/arrays.py
@@ -0,0 +1,13 @@
+# Creating an array (list)
+array = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print(array[0])  # Output: 1
+
+# Inserting an element
+array.append(6)  # [1, 2, 3, 4, 5, 6]
+
+# Deleting an element
+array.remove(3)  # [1, 2, 4, 5, 6]
+
+print(array)
